66f216b4ede8b5836899a39a20299871667c196b
Karisma-Dana <karisma.dana2007@gmail.com>
2026-07-01 10:38:31 +0800

    Initial commit - Setup Project

---
+++ src/main/java/com/mycompany/store_app/STORE_APP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.store_app;
+
+/**
+ *
+ * @author karis
+ */
+public class STORE_APP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
