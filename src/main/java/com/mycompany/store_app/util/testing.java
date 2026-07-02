c08d60abaee5108ec6975450e1cdc0497aac0021
Karisma-Dana <karisma.dana2007@gmail.com>
2026-07-02 23:45:27 +0800

    email Auth

---
+++ src/main/java/com/mycompany/store_app/util/testing.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.store_app.util;
+
+/**
+ *
+ * @author karis
+ */
+public class testing {
+    private EmailServices email = new EmailServices(); 
+    public void main(){
+        email.sendEmailOTP("gptfamily09@gmail.com", "909323");
+    }
+    
+}
