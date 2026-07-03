01ddb12d0c03b56a7734fbb78100817f3f716d32
Karisma-Dana <karisma.dana2007@gmail.com>
2026-07-04 02:12:12 +0800

    UI dashbord, UI barang 80 %, UI inser/Update barang, UI insert/UpdateVoucher

--- src/main/java/com/mycompany/store_app/util/testing.java
+++ src/main/java/com/mycompany/store_app/util/testing.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.store_app.util;
+import java.util.Random;
 
 /**
  *
@@ -11,7 +12,20 @@ package com.mycompany.store_app.util;
 public class testing {
     private EmailServices email = new EmailServices(); 
     public void main(){
-        email.sendEmailOTP("gptfamily09@gmail.com", "909323");
+        generateOTP();
+    }
+    
+    
+    public String generateOTP(){
+        String OTP = "";
+        Random randomNumber = new Random();
+        
+        for (int i = 0; i < 5; i++){
+            int random = randomNumber.nextInt(9);
+            OTP = OTP + random;
+        }
+        System.out.println(OTP);
+        return OTP;
     }
     
 }
